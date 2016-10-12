90d44357079a19d4b72ddb754272c90b26f8fd41
Narrev <NarrevRBX@gmail.com>
2016-10-11 20:45:37 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,34 +1,34 @@
-return function(WasHttpEnabled)
-	local HttpService = game:GetService("HttpService")
-	local ReplicatedStorage = game:GetService("ReplicatedStorage")
-	local ServerScriptService = game:GetService("ServerScriptService")
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
 
-	local Modules = {
-		Nevermore = "https://raw.githubusercontent.com/Narrev/NevermoreEngine/c6b971b4c925a251ae1ab4118992bc7c09f11cdc/App/NevermoreEngine.lua";
-		Emoji = "https://github.com/Narrev/RavenChat/blob/master/Emoji.module.lua";
-		List = "https://github.com/Narrev/RavenChat/blob/master/List.module.lua";
-		RavenChat = "https://github.com/Narrev/RavenChat/blob/master/RavenChat.module.lua";
-	}
+local Modules = {
+	Nevermore = "https://raw.githubusercontent.com/Narrev/NevermoreEngine/c6b971b4c925a251ae1ab4118992bc7c09f11cdc/App/NevermoreEngine.lua";
+	Emoji = "https://raw.githubusercontent.com/Narrev/RavenChat/master/Emoji.module.lua";
+	List = "https://raw.githubusercontent.com/Narrev/RavenChat/master/List.module.lua";
+	RavenChat = "https://raw.githubusercontent.com/Narrev/RavenChat/master/RavenChat.module.lua";
+}
 
-	local function GetFolder(Name, Parent)
-		local Folder = Parent:FindFirstChild(Name)
-		if not Folder then
-			Folder = Instance.new("Folder", Parent)
-		end
-		return Folder
+local function GetFolder(Name, Parent)
+	local Folder = Parent:FindFirstChild(Name)
+	if not Folder then
+		Folder = Instance.new("Folder", Parent)
 	end
+	return Folder
+end
 
-	local ModuleScript = {
-		new = function(Name, Parent)
-			local script = Instance.new("ModuleScript", Parent)
-			script.Name = Name
-			script.Source = HttpService:GetAsync(Modules[Name])
-			return script
-		end;
-	}
+local ModuleScript = {
+	new = function(Name, Parent)
+		local newScript = Instance.new("ModuleScript", Parent)
+		newScript.Name = Name
+		newScript.Source = HttpService:GetAsync(Modules[Name])
+		return newScript
+	end;
+}
 
-	local ModuleFolder = GetFolder("Modules", ServerScriptService)
+local ModuleFolder = GetFolder("Modules", ServerScriptService)
 
+return function(WasHttpEnabled)
 	ModuleScript.new("Nevermore", ReplicatedStorage)
 	ModuleScript.new("Emoji", ModuleFolder)
 	ModuleScript.new("List", ModuleFolder)
